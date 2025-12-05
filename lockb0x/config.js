711ee8add10e4dba1c28a563190f57e1b457d7f9
steven-tomlinson <steven@thefirm.codes>
2025-12-05 09:38:51 -0800

    fix: Correct formatting of POH_API_BASE URL in configuration

--- lockb0x/config.js
+++ lockb0x/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  POH_API_BASE: "https://poh-api.linea.build/poh/v2/"
+  POH_API_BASE: "https://poh-api.linea.build/poh/v2/",
   // Add other public config values here as needed
 };
